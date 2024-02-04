e7eb449a4f3f70f777ac1b9e6ce5446693810b1b
Nehadobariya1 <143496591+Nehadobariya1@users.noreply.github.com>
2024-02-04 16:53:43 +0000

    container config done

---
+++ basics_of_js/variable.js
@@ -0,0 +1,12 @@
+const accountID = 156182
+let accountEmail = "abc@gmail.com"
+var accountPassword = "17548"
+accountCity = "Delhi"
+
+//accountId = 75245 //Can't reasign value in const
+accountEmail="nmd@gmail.com"
+accountPassword="489515"
+accountCity="Lucknow"
+
+console.log(accountID);
+console.table([accountID, accountEmail, accountPassword, accountCity])
