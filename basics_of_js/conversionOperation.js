436b8fdc0bcafb21bc22d0ecc765e035d0f49199
Nehadobariya1 <143496591+Nehadobariya1@users.noreply.github.com>
2024-02-05 17:35:26 +0000

    Add

---
+++ basics_of_js/conversionOperation.js
@@ -0,0 +1,31 @@
+
+
+let age = 25
+console.log(age)
+
+//Know the type 
+console.log(typeof(age)) //number
+
+let score = "155"
+console.log(typeof score) //string
+
+let time = "12PM"
+console.log(typeof time) //now type is string
+
+let rightTime = Number(time)
+console.log(rightTime);         //NaN (not a Number)
+console.log(typeof rightTime)  //Now it's number
+
+let isLoggedIn = 1      //true
+let booleaneLoggrdIn = Boolean(isLoggedIn)
+console.log(booleaneLoggrdIn);
+
+let isAvailable = 0     //false
+let booleanAvailable = Boolean(isAvailable)
+console.log(booleanAvailable);
+
+// 1=>true; 0=>false
+// ""=>false            //Empty string return false in boolean
+// "Abc"=>true          //value in string return true in boolean
+
+
--- basics_of_js/datatypes.js
+++ basics_of_js/datatypes.js
@@ -13,3 +13,7 @@ let mobileNo = null     //null => standalone value
 //null is object
 
 //console.table([name1,age,isAvailable,mobileNo]);
+
+//------------------------------------TYPES-----------------------------------
+//1.primitive datatype => 7types : String, Number,Boolean, Null, undefined, symbol, BigInt 
+//2.Non-primitive datatype ( Refrence ) => 3types: Array, Objects, function
