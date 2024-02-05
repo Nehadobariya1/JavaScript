a7013eaa3362d5c64c73256eb0d6e23d4218c25a
Nehadobariya1 <143496591+Nehadobariya1@users.noreply.github.com>
2024-02-05 11:05:32 +0000

    container configue done

---
+++ basics_of_js/datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all js code as new version
+
+let name = "john" //string => ""
+let age = 25 //number (for large Number 'bigint')
+let isAvailable = true //boolean => true/false
+let state; //undefined
+let mobileNo = null //null => standalone value
+//null is object
